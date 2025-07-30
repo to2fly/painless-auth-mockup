c107a8078ca7613bebc6687016728d3df5e88136
to2fly <53888338+to2fly@users.noreply.github.com>
2025-07-30 17:49:31 -0400

    Initial upload

---
+++ src/mockData.js
@@ -0,0 +1,13 @@
+
+export const authCodes = {
+  "123456789": {
+    image: "/images/item1.jpg",
+    description: "Red hoodie from Drop 1",
+    uses: 0
+  },
+  "987654321": {
+    image: "/images/item2.jpg",
+    description: "Black tee from Drop 2",
+    uses: 0
+  }
+};
